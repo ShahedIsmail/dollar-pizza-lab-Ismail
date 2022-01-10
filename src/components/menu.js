6449dd10985930067b378a1c9cdd7aebcc52f179
ShahedIsmail <sismail3391@richmondhillhs.org>
2022-01-10 16:28:33 +0000

    DOLLARPIZZA

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Plain from './plain';
-import Pepperoni from './pepperoni';
-import MeatLover from './meat-lover';
-import Veggie from './veggie';
-import Hawaiian from './hawaiian';
-import Bbq from './bbq';
+import Pepperoni from './images/pepperoni.jpg';
+import MeatLover from './images/meat.jpg';
+import Veggie from './images/veggie.jpeg';
+import Hawaiian from './images/hawaiian.jpg';
+import Bbq from './images/bbq.jpg';
+import plain from './images/plain.jpg';
+import Slice from './slice'
 import './css/menu.css';
 
 function Menu() {
@@ -13,15 +14,39 @@ function Menu() {
       <h1 className='primary-text'>New York's World Famous</h1>
       <h1 className='secondary-text'>Pizza Menu</h1>
       <div className='row'>
-        <Plain/>
-        <Pepperoni/>
-        <MeatLover/>
-        <Veggie/>
-        <Hawaiian/>
-        <Bbq/>
+       <Slice 
+       img= {plain}
+       title= "plain"
+       description="It's got cheese, it's got tomato sauce, and it's only  a dollar. What more could you ask for?"
+       price="$1.00" />
+       <Slice 
+       img={Bbq}
+       title="BBQ"
+       description="Perhaps the most popular slicewe offer. BBQ chicken over a delicious dollar pizza. What more could one want in life ?."
+       price="$2.00"/>
+       <Slice 
+       img={Pepperoni}
+       description= "It's got cheese, it's got tomato sauce, and it's got pepperoni. One of the best topping you could hope for."
+       price= "$5.00"/>
+       <Slice 
+       img= {Hawaiian}
+       title= "Hawaiian"
+       description="We know, you think this is gross. But try this once and we swear you'll be hooked on this pineapple and ham pizza."
+       price="$2.00" />
+       <Slice 
+       img= {Veggie}
+       title= "Veggie"
+       description="You trying to be health but you still can't resist pizza ? Well then the veggie slice is for you! This is gluit-free pizza at it's finest."
+       price="$3.00" />
+       <Slice 
+       img= {MeatLover}
+       title= "Meat Lover"
+       description="pepperoni not enough meat for you ? Well this slice has pepperoni, meatbslls, and sausage."
+       price="$10.00" />
       </div>
     </div>
   );
 }
 
 export default Menu;
+
